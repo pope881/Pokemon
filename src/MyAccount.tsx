be49d63e9b109999014c60e9e7d91ca1649c11fe
Bartosz Papierz <papierz.bartosz@gmail.com>
2024-07-06 17:40:52 +0200

    MyAccount.tsx file has been changed

--- src/MyAccount.tsx
+++ src/MyAccount.tsx
@@ -1,7 +1,7 @@
 export const MyAccount = () => {
 	return (
 		<>
-			<div className="flex justify-center items-center h-44 bg-red-600">
+			<div className="flex justify-center items-center h-44 bg-amber-600">
 				<p>MY ACCOUNT</p>
 			</div>
 		</>
